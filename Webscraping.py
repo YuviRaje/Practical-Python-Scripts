6d78e860357c34ddb8f10b1ff25b7ae93f0d280f
Yuvraj Jadhav <yuvrajjadhav@gmail.com>
2016-04-17 23:51:18 -0600

    Adding web scraping scripts

---
+++ Webscraping.py
@@ -0,0 +1,16 @@
+# webscraping example
+#!/usr/bin/python env
+from bs4 import BeautifulSoup
+
+import requests
+
+url = input("Enter a website to extract the URL's from: ")
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
